3efa962eb5e44fae4d8123db41a2ac45d7d63ed4
drillprop <drylbartosz@gmail.com>
2020-10-10 16:24:59 +0200

    create shopSlice

---
+++ src/redux/shop/shopSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+import items from '../../data/items.json';
+
+const initialState = items;
+export type ProductType = typeof initialState[0];
+
+export const shopSlice = createSlice({
+  name: 'shop',
+  initialState,
+  reducers: {},
+});
+
+export default shopSlice.reducer;
