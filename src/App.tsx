0ccadcf08ea77ce712e66785364a4c154241fc24
drillprop <drylbartosz@gmail.com>
2020-10-11 23:56:49 +0200

    style Shop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from './components/NavBar/NavBar';
 import Shop from './views/Shop/Shop';
 
 function App() {
   return (
     <>
+      <NavBar />
       <Shop />
     </>
   );
--- src/views/Shop/Shop.tsx
+++ src/views/Shop/Shop.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Cart from '../../components/Cart/Cart';
 import Products from '../../components/Products/Products';
+import { RootState } from '../../redux/store';
 import styles from './Shop.module.scss';
 
 const Shop = () => {
+  const isCartVisible = useSelector<RootState>(
+    ({ menu }) => menu.isCartVisible
+  );
   return (
     <div className={styles.wrapper}>
       <Products />
-      <Cart />
+      {isCartVisible && <Cart />}
     </div>
   );
 };
