ee0aa14b53cd883a09b2ca18e4f2251c710fff8e
drillprop <drylbartosz@gmail.com>
2020-10-11 23:55:52 +0200

    add cartLength to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import styles from './NavBar.module.scss';
 import logo from '../../assets/logo.png';
 import { ReactComponent as CartIcon } from '../../assets/cart.svg';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleCart } from '../../redux/menu/menuSlice';
+import { RootState } from '../../redux/store';
 
 const NavBar = () => {
   const dispatch = useDispatch();
+  const cartLength = useSelector<RootState, number>(({ cart }) =>
+    cart.reduce((acc, current) => {
+      acc += current.quantity;
+      return acc;
+    }, 0)
+  );
   return (
     <nav className={styles.nav}>
       <h1>
@@ -19,6 +26,7 @@ const NavBar = () => {
         onClick={() => dispatch(toggleCart())}
       >
         <CartIcon />
+        <span className={styles.itemsInCart}>{cartLength}</span>
       </button>
     </nav>
   );
